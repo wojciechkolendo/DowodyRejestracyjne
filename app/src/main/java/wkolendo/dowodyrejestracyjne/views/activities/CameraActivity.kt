caa86140e9978bb4e5dbe35d662b607ebf68aaec
Wojciech Kolendo <kolendo.wojciech@gmail.com>
2018-09-06 20:18:02 +0200

    camera

--- app/src/main/java/wkolendo/dowodyrejestracyjne/views/activities/CameraActivity.kt
+++ app/src/main/java/wkolendo/dowodyrejestracyjne/views/activities/CameraActivity.kt
@@ -1,9 +1,45 @@
 package wkolendo.dowodyrejestracyjne.views.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.os.Bundle
+import software.rsquared.permissiontools.OnPermissionResultTask
+import software.rsquared.permissiontools.Permissions
+import wkolendo.dowodyrejestracyjne.R
+import wkolendo.dowodyrejestracyjne.utils.camera.CameraManager
 
 /**
  * @author Wojtek Kolendo
  */
-class CameraActivity : AppCompatActivity() {
+class CameraActivity : DowodyRejestracyjneActivity() {
+
+	private var cameraManager: CameraManager? = null
+
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_camera)
+	}
+
+	override fun onResume() {
+		super.onResume()
+		checkCameraPermission()
+	}
+
+	private fun checkCameraPermission() {
+		Permissions.checkPermission(this, object : OnPermissionResultTask() {
+			@Throws(SecurityException::class)
+			override fun onPermissionGranted(permissions: Array<String>) {
+				initCamera()
+			}
+		}, Manifest.permission.CAMERA)
+	}
+
+	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+		Permissions.onRequestPermissionsResult(requestCode, permissions, grantResults);
+	}
+
+	private fun initCamera() {
+
+	}
 }
---
+++ app/src/main/java/wkolendo/dowodyrejestracyjne/views/activities/DowodyRejestracyjneActivity.kt
@@ -0,0 +1,56 @@
+package wkolendo.dowodyrejestracyjne.views.activities
+
+import android.view.ViewGroup
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import wkolendo.dowodyrejestracyjne.R
+import wkolendo.dowodyrejestracyjne.views.dialogs.LoadingDialog
+
+/**
+ * @author Wojtek Kolendo
+ */
+abstract class DowodyRejestracyjneActivity : AppCompatActivity() {
+
+	var loadingDialog: LoadingDialog? = null
+
+	private fun showSnackMessage(snackbar: Snackbar): Snackbar {
+		snackbar.show()
+		return snackbar
+	}
+
+	fun showSnackMessage(@StringRes msg: Int): Snackbar {
+		return showSnackMessage(msg, Snackbar.LENGTH_LONG)
+	}
+
+	fun showSnackMessage(@StringRes msg: Int, duration: Int): Snackbar {
+		return showSnackMessage(Snackbar.make(getRootView(), msg, duration))
+	}
+
+	fun showSnackMessage(msg: String): Snackbar {
+		return showSnackMessage(msg, Snackbar.LENGTH_LONG)
+	}
+
+	fun showSnackMessage(msg: String, duration: Int): Snackbar {
+		return showSnackMessage(Snackbar.make(getRootView(), msg, duration))
+	}
+
+	open fun showLoading(show: Boolean) {
+		if (show) {
+			if (loadingDialog == null) {
+				loadingDialog = LoadingDialog(this, R.style.Theme_DowodyRejestracyjne_ProgressDialog)
+			}
+			if (!loadingDialog!!.isShowing) {
+				loadingDialog?.show()
+			}
+		} else {
+			if (loadingDialog != null && loadingDialog!!.isShowing) {
+				loadingDialog?.dismiss()
+			}
+		}
+	}
+
+	fun getRootView(): ViewGroup {
+		return this.findViewById(android.R.id.content) as ViewGroup
+	}
+}
